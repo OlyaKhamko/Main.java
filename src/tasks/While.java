fd88af03ab006605cf2a0b520c418b65c3854b02
OlyaKhamko <volhakhamko@gmail.com>
2021-07-11 17:06:52 +0300

    Init

---
+++ src/tasks/While.java
@@ -0,0 +1,23 @@
+package tasks;
+
+public class While {
+//    Необходимо, чтоб программа выводила на экран вот такую последовательность:
+//            7 14 21 28 35 42 49 56 63 70 77 84 91 98
+//    public static void main (String[] args){
+//        int a = 7;
+//        while (a<=98){
+//            System.out.print(a+" ");
+//            a+=7;
+//        }
+//    }
+//Необходимо вывести на консоль такую последовательность чисел:
+//            1 2 4 8 16 32 64 128 256 512
+//    public static void main(String[] args){
+//        int a = 1;
+//        while (a<=512){
+//            System.out.print(a + " ");
+//            a*=2;
+//        }
+//    }
+
+}
