a02b9ed1a238f06352b0091f46c27865b31a28aa
OlyaKhamko <volhakhamko@gmail.com>
2021-07-11 16:56:10 +0300

    Init

---
+++ src/tasks/Сycles.java
@@ -0,0 +1,40 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Сycles {
+
+//        Необходимо вывести на экран числа от 1 до 5. На экране должно быть:
+//            1 2 3 4 5
+    public static void main (String[] args) {
+        for (int i=1; i<=5; i++ ) {
+            System.out.print(i + "");
+        }
+    }
+
+//        Необходимо вывести на экран числа от 5 до 1. На экране должно быть:
+//            5 4 3 2 1
+//    public static void main (String[]args){
+//        for (int i=5; i>=1; i--){
+//            System.out.print(i + "");
+//        }
+//    }
+//}
+//Необходимо вывести на экран таблицу умножения на 3:
+//    public static void main (String[] args){
+//    for(int i=1; i<=10; i++){
+//        System.out.println("3*" + i + "=" + i*3);
+//    }
+//    }
+//Напишите программу, где пользователь вводит любое целое положительное число.
+//    А программа суммирует все числа от 1 до введенного пользователем числа.
+//    public static void main(String[] args){
+//        System.out.print("Введите любое целое число: ");
+//        Scanner scn = new Scanner(System.in);
+//        int n = scn.nextInt();
+//        int sum = 0;
+//        for (int i=1; i<=n; i++)
+//        sum = sum + i;
+//        System.out.println(sum);
+//    }
+}
